c2a1bff292285cbd3925f40c8c76c4249bd8a048
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-19 11:34:19 -0300

    Update frases.c

---
+++ frases.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+    char frases [50];
+    int qtdfrases = 5;
+
+    printf("*****Adicione suas frases*****\n");
+
+    for (int i = 1; i <= qtdfrases; i++)
+    {
+        printf("Adicione a %i frase\n", i);
+        scanf("%s", frases);
+    }
+    
+
+
+
+
+
+
+
+}
