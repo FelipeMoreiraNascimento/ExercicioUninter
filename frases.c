cbd93b3070754c15016328a1c7ffef64621959b5
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-19 21:19:31 -0300

    Update frases.c

--- frases.c
+++ frases.c
@@ -8,15 +8,27 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
     char frase1 [50];
-   
 
     printf("Adicione a  frase\n");
     gets(frase1);
 
-    for (int i = 0; i < strlen(frase1); i++)
-    {
-        printf("%c", toupper(frase1[i]));
-    }
+
+        FILE * f;
+
+        f = fopen("frases.txt", "r+");
+        int qtd;
+        fscanf(f, "%d", &qtd);
+        qtd++;
+
+        fseek(f, 0, SEEK_SET);
+        fprintf(f, "%d", qtd);
+
+        fseek(f, 0, SEEK_END);
+
+        fprintf(f, "\n%s", frase1);
+
+        fclose(f);
+    
 
     
     
