d3b5bca37d68b9f8bd16450f7859893cffbcd3c9
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-21 12:21:40 -0300

    Update frases.c

--- frases.c
+++ frases.c
@@ -4,35 +4,52 @@
 #include<ctype.h>
 #include<locale.h>
 
+char maiusculas [5][50];
+char frase1 [5][50];
+
+
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    char frase1 [50];
 
-    printf("Adicione a  frase\n");
-    gets(frase1);
+    char nomearquivo[15];
+    printf("Digite o nome do arquivo: ");
+    gets(nomearquivo);
+    strcat(nomearquivo, ".txt");
 
+    for (int i = 1; i <= 5 ; i++)
+    {
+        printf("Adicione a frase %i \n", i);
+        gets(frase1[i]);
+    }
 
-        FILE * f;
-
-        f = fopen("frases.txt", "r+");
-        int qtd;
-        fscanf(f, "%d", &qtd);
-        qtd++;
-
-        fseek(f, 0, SEEK_SET);
-        fprintf(f, "%d", qtd);
-
-        fseek(f, 0, SEEK_END);
-
-        fprintf(f, "\n%s", frase1);
-
-        fclose(f);
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < 50; j++)
+        {
+            maiusculas[i][j] = toupper(frase1[i][j]);
+        }    
+    }
     
 
-    
-    
-   
+    FILE * f;
 
-   
+    f = fopen(nomearquivo, "r+");
+    int qtd;
+    fscanf(f, "%d", &qtd);
+    qtd++;
+
+    fseek(f, 0, SEEK_SET);
+    fprintf(f, "%d", qtd);
+
+    fseek(f, 0, SEEK_END);
+
+    for (int i = 1; i <= 5 ; i++)
+    {
+        fprintf(f, "\n%s", maiusculas[i]);
+    }
+
+    fclose(f);
+
 }
+
